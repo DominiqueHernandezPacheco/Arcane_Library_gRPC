11af19f439c1792e9d27d09236200add6fee0602
Dominique Hernández <148931049+DominiqueHernandezPacheco@users.noreply.github.com>
2026-03-12 13:57:50 -0600

    Update ngrok URL for remote connection

--- cliente_ui.py
+++ cliente_ui.py
@@ -35,7 +35,7 @@ if 'productos_lista' not in st.session_state:
     st.session_state.productos_lista = []
 
 # --- CONFIGURACIÓN DE CONEXIÓN REMOTA ---
-url_ngrok_activa = "6.tcp.ngrok.io:13299" 
+url_ngrok_activa = "4.tcp.ngrok.io:11767" 
 
 with st.sidebar:
     st.markdown("### 🌐 Monitoreo Distribuido")
